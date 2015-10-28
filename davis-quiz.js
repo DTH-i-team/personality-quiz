d229169c08985cf1b633d231bb0b83ae85000d7f
Kammy Liu <aila293@users.noreply.github.com>
2015-10-27 21:27:41 -0400

    fixed syntax

--- davis-quiz.js
+++ davis-quiz.js
@@ -1,17 +1,17 @@
 quiz_data = {
 
     "title": "What floor of Davis library should you study on?",
-    "description": "When the UL is too busy to focus, the study lounge is occupied with other chatty students, and, there’s only one other place to go for students looking to get their lives together: Davis library. In a place so monstrous, it can be hard to decide where to set up camp. Take this quiz to find out what floor of Davis library will work best for you.",
+    "description": "When the UL is too busy to focus, the study lounge is occupied with other chatty students, and, there's only one other place to go for students looking to get their lives together: Davis library. In a place so monstrous, it can be hard to decide where to set up camp. Take this quiz to find out what floor of Davis library will work best for you.",
 
     "results": [
-        {"label": "Don’t study in Davis - you’re doin’ alright, kid!",
-        "description": "You might not need the intensity of Davis’s study environment right now - either that, or your answers have revealed to us that while you say you’re going there to be productive, we both know that you’re not really gonna go there and be productive. Run off some energy, smile and laugh while you can, and then, once you really don’t have it together, come back to this quiz again.
+        {"label": "Don't study in Davis - you're doin' alright, kid!",
+        "description": "You might not need the intensity of Davis's study environment right now - either that, or your answers have revealed to us that while you say you're going there to be productive, we both know that you're not really gonna go there and be productive. Run off some energy, smile and laugh while you can, and then, once you really don't have it together, come back to this quiz again.
 ", 
         "img_src": "http://www.sure-sense.com/BS_Site/Images/Trim%20Stock%20Throwing%20Paper%20in%20the%20Air2.jpg"
 	   "text": "Source: Sinscaptain on Blogspot",
         },
         
-        {"label": "Floor 1-3: Wow, I didn’t even know it was legal for a math equation to be that long.",
+        {"label": "Floor 1-3: Wow, I didn't even know it was legal for a math equation to be that long.",
         "description": "Your stress levels are definitely high enough that you need a place to focus, but not necessarily somewhere with absolute silence. The best places for you to work are probably on the first couple floors of Davis, where you can ask your study buddies a question without being brutally shushed.", 
         "img_src": "http://www.insidearm.com/wp-content/uploads/lecture1.jpg?279849"
 	   "text": "Source: Inside Arm",
@@ -23,7 +23,7 @@ quiz_data = {
 	   "text": "Students 4 Best Evidence",
         },
         
-        {"label": "Floor 7-8: What’s that, you say? An 8 AM test you have to ace to pass the class?",
+        {"label": "Floor 7-8: What's that, you say? An 8 AM test you have to ace to pass the class?",
         "description": "You must have complete and utter silence. If anyone is talking, they will be kicked off the island. If they are breathing too loudly, they will be escorted out by your personal bouncers.  You must be able to hear paint drying for the focus that you require. Head up to the top levels of the library ASAP, and best of luck on whatever insane assignment you have due tomorrow.", 
         "img_src": "http://lh5.ggpht.com/-XNqYfN-rTLQ/UwKSWCsurrI/AAAAAAAAXGA/kMhLqflOH-Q/beating%252520stress%252520-%252520law%252520student_thumb%25255B5%25255D.jpg?imgmax=800"
 	   "text": "Source: Law Actually on Blogspot",
@@ -31,12 +31,12 @@ quiz_data = {
     ],
         
     "questions": [
-        {"question": "How hard is the class you’re trying to study for?", 
+        {"question": "How hard is the class you're trying to study for?", 
          "answers": [
              {"text": "My blind and deaf grandmother could ace this class.", 
               "effect": [1,0,0,0]
              },
-             {"text": "It can be difficult at times, but it hasn’t destroyed my sleeping schedule quite yet.", 
+             {"text": "It can be difficult at times, but it hasn't destroyed my sleeping schedule quite yet.", 
               "effect": [0,1,0,0]
              },
              {"text": "I have a designated cry zone specifically for this course.", 
@@ -114,7 +114,7 @@ quiz_data = {
              {"text": "Yes, but I have a good excuse!", 
               "effect": [0,0,1,0]
              },
-             {"text": "I’ll answer this question later, I swear.", 
+             {"text": "I'll answer this question later, I swear.", 
               "effect": [1,0,0,0]
              }
          ], 
