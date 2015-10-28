4ab01d4338694c1ed41142a6843a1c88d78c0a6d
blahblahkams@yahoo.com <blahblahkams@yahoo.com>
2015-10-27 20:57:35 -0400

    data for davis library quiz

---
+++ davis-quiz.js
@@ -0,0 +1,161 @@
+quiz_data = {
+
+    "title": "What floor of Davis library should you study on?",
+    "description": "When the UL is too busy to focus, the study lounge is occupied with other chatty students, and, there’s only one other place to go for students looking to get their lives together: Davis library. In a place so monstrous, it can be hard to decide where to set up camp. Take this quiz to find out what floor of Davis library will work best for you.",
+
+    "results": [
+        {"label": "Don’t study in Davis - you’re doin’ alright, kid!",
+        "description": "You might not need the intensity of Davis’s study environment right now - either that, or your answers have revealed to us that while you say you’re going there to be productive, we both know that you’re not really gonna go there and be productive. Run off some energy, smile and laugh while you can, and then, once you really don’t have it together, come back to this quiz again.
+", 
+        "img_src": "http://www.sure-sense.com/BS_Site/Images/Trim%20Stock%20Throwing%20Paper%20in%20the%20Air2.jpg"
+	   "text": "Source: Sinscaptain on Blogspot",
+        },
+        
+        {"label": "Floor "1-3: Wow, I didn’t even know it was legal for a math equation to be that long.",
+        "description": "Your stress levels are definitely high enough that you need a place to focus, but not necessarily somewhere with absolute silence. The best places for you to work are probably on the first couple floors of Davis, where you can ask your study buddies a question without being brutally shushed.", 
+        "img_src": "http://www.insidearm.com/wp-content/uploads/lecture1.jpg?279849"
+	   "text": "Source: Inside Arm",
+        },
+        
+         {"label": "Floor 4-6: So, I see you forgot that eight-page English paper was due tomorrow.",
+        "description": "Time to buckle down and get things done. These floors are for those with high stress and low distraction tolerance. Once, I was on the fifth floor of Davis and heard somebody get shushed when their sneaker squeaked on the tile, so you can tell that the people here are ready to shut up, sit down, and devote themselves to an evening of hard work and possible crying.", 
+        "img_src": "http://www.students4bestevidence.net/wp-content/uploads/2014/08/stressed-student.jpg"
+	   "text": "Students 4 Best Evidence",
+        },
+        
+        {"label": "Floor "7-8: What’s that, you say? An 8 AM test you have to ace to pass the class?",
+        "description": "You must have complete and utter silence. If anyone is talking, they will be kicked off the island. If they are breathing too loudly, they will be escorted out by your personal bouncers.  You must be able to hear paint drying for the focus that you require. Head up to the top levels of the library ASAP, and best of luck on whatever insane assignment you have due tomorrow.", 
+        "img_src": "http://lh5.ggpht.com/-XNqYfN-rTLQ/UwKSWCsurrI/AAAAAAAAXGA/kMhLqflOH-Q/beating%252520stress%252520-%252520law%252520student_thumb%25255B5%25255D.jpg?imgmax=800"
+	   "text": "Source: Law Actually on Blogspot",
+        }
+    ],
+        
+    "questions": [
+        {"question": "How hard is the class you’re trying to study for?", 
+         "answers": [
+             {"text": "My blind and deaf grandmother could ace this class.", 
+              "effect": [1,0,0,0]
+             },
+             {"text": "It can be difficult at times, but it hasn’t destroyed my sleeping schedule quite yet.", 
+              "effect": [0,1,0,0]
+             },
+             {"text": "I have a designated cry zone specifically for this course.", 
+              "effect": [0,0,1,0]
+             }
+		   {"text": "The top student just got a 47% on the midterm.", 
+              "effect": [0,0,0,1]
+             },
+         ], 
+         "img_src": "http://images.clipartpanda.com/rainbow-with-clouds-clipart-jTxE6jrec.jpeg"
+        },
+        {"question": "When you study, you like to eat _______", 
+         "answers": [
+             {"text": "a "big bag of potato chips.", 
+              "effect": [1,0,0,0]
+             },
+             {"text": "fruit, like an apple or a banana.", 
+              "effect": [0,1,0,0]
+             }
+		   {"text": "chewing gum.", 
+              "effect": [0,0,1,0]
+             }
+		   {"text": "nothing - food distracts me from focusing.", 
+              "effect": [0,0,0,1]
+             },
+         ], 
+         "img_src": "http://i5.walmartimages.com/dfw/dce07b8c-2d8d/k2-_3fe71aae-bec7-4700-92c2-9292e4ab16b2.v1.jpg"
+        },        
+        {"question": "Pick an emoji.", 
+         "answers": [
+             {"img_src":"http://emojipedia-us.s3.amazonaws.com/cache/a8/eb/a8eb24aefb9b1b9ac21a6aaf97d0869a.png"
+		   "text": "Source: Emojipedia", 
+              "effect": [0,0,1,0]
+             },
+             {"img_src":"http://emojipedia-us.s3.amazonaws.com/cache/a1/9e/a19e9a60979e3eae74f73f19462c00c0.png"
+		   "text": "Source: Emojipedia", 
+              "effect": [1,0,0,0]
+             },
+             {"img_src":"http://emojipedia-us.s3.amazonaws.com/cache/de/ca/decadd7edb6b1014ca0cb7a1afcb8ea3.png"
+		    "text": "Source: Emojipedia", 
+              "effect": [0,1,0,0]
+             },
+             {"img_src":"http://emojipedia-us.s3.amazonaws.com/cache/5b/d2/5bd298c3f0d858aef9127738e89f16ad.png"
+		   "text": "Source: Emojipedia", 
+              "effect": [0,0,0,1]
+             }
+         ], 
+         "img_src": "https://upload.wikimedia.org/wikipedia/commons/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg"
+   },        
+        {"question": "How late do you plan on going to sleep tonight?", 
+         "answers": [
+             {"text": "9 pm", 
+              "effect": [1,0,0,0]
+             },
+             {"text": "11 pm", 
+              "effect": [0,1,0,0]
+             },
+             {"text": "12:30 am", 
+              "effect": [0,0,1,0]
+             },
+             {"text": "1 am or later", 
+              "effect": [0,0,0,1]
+             }
+         ], 
+"img_src": "http://i5.walmartimages.com/dfw/dce07b8c-2d8d/k2-_3fe71aae-bec7-4700-92c2-9292e4ab16b2.v1.jpg"
+        },        
+        {"question": "Have you been procrastinating lately?", 
+         "answers": [
+             {"text": "Of course not!", 
+              "effect": [0,0,0,1]
+             },
+             {"text": "Maybe a little bit.", 
+              "effect": [0,1,0,0]
+             },
+             {"text": "Yes, but I have a good excuse!", 
+              "effect": [0,0,1,0]
+             },
+             {"text": "I’ll answer this question later, I swear.", 
+              "effect": [1,0,0,0]
+             }
+         ], 
+"img_src": "http://i5.walmartimages.com/dfw/dce07b8c-2d8d/k2-_3fe71aae-bec7-4700-92c2-9292e4ab16b2.v1.jpg"
+        },        
+        {"question": "Which one of these dog photos looks most like you right now?", 
+         "answers": [
+             {"img_src": "https://41.media.tumblr.com/47c611bc6e5071b0f29f6ca09d00e2e3/tumblr_mwl565qsZA1r6y2r7o1_400.jpg",
+		   "text": "Source: Tumblr", 
+              "effect": [0,0,0,1]
+             },
+             {"img_src": "http://media.techeblog.com/images/maru-the-dog.jpg",
+		    "text": "Source: Techeblog", 
+              "effect": [0,1,0,0]
+             },
+             {"img_src": "http://i.imgur.com/59iGWch.jpg",
+		    "text": "Source: Imgur", 
+              "effect": [0,0,1,0]
+             },
+             {"img_src": "http://img14.deviantart.net/bbbe/i/2015/089/b/e/happy_husky__read_description_by_sparksy2015-d8nr3yk.jpg",
+		    "text": "Source: Deviant Art", 
+              "effect": [1,0,0,0]
+             }
+         ], 
+"img_src": "http://i5.walmartimages.com/dfw/dce07b8c-2d8d/k2-_3fe71aae-bec7-4700-92c2-9292e4ab16b2.v1.jpg"
+        },        
+        {"question": "If you could be doing anything other than studying, you would be ________", 
+         "answers": [
+             {"text": "eating on Franklin St.", 
+              "effect": [0,0,1,0]
+             },
+             {"text": "sleeping.", 
+              "effect": [0,0,0,1]
+             },
+             {"text": "partying!", 
+              "effect": [1,0,0,0]
+             },
+             {"text": "watching Netflix (chill optional).", 
+              "effect": [0,1,0,0]
+             }
+         ],
+        }
+    ]
+};
