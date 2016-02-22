6d74d776c872c79a95e54c8e610e61005cad2a40
mscruser <mscruser@gmail.com>
2016-02-21 20:18:28 -0500

    first

---
+++ mourn-duke.js
@@ -0,0 +1,358 @@
+/*
+
+INSTRUCTIONS:
+
+This template will be processed by a computer program,
+
+so formatting is VERY important.
+
+Make sure not to delete or switch around any quotes, commas, colons,
+
+parentheses, or brackets.
+
+Do not start new lines (i.e. type Enter) inside a set of double?quotes.
+
+Only start new lines when copying?pasting or on a blank line.
+
+Anything inside these symbols:
+
+/ * * /
+
+is an instruction. Do not write any quiz information there
+
+because the program won't look at it.
+
+*/
+
+quiz_data = {
+
+/*
+
+This section is the title and description for the whole quiz.
+
+It'll be shown at the top of the quiz page.
+
+Write in your own title and description.
+
+MAKE SURE to keep the quotes at the beginning and end,
+
+and keep the comma at the end of the line.
+
+*/
+
+"title": "How should you mourn the Duke loss?",
+
+"description": "We all have to mourn but each sign mourns differently?",
+
+/*
+
+This section lists all of the possible results of the quiz.
+
+When the reader finishes, one of these results will be shown.
+
+*/
+
+"results": [
+
+/* Result 1 */
+
+{
+
+"title": "Aries",
+
+"description": "Eat some cake!",
+
+
+},
+
+/* Result 2 */
+
+{
+
+"title": "Taurus",
+
+"description": "Light a candle and say a prayer.",
+
+
+
+},
+
+/* Result 3 */
+
+{
+
+"title": "Gemini",
+
+"description": "Skip your Friday classes!",
+
+
+
+},
+
+/* Result 4 */
+
+{
+
+"title": "Cancer",
+
+"description": "Sing the song of your people at midnight for 5 days",
+
+
+
+},
+
+/* Result 5*/
+
+{
+
+"title": "Leo",
+
+"description": "Ben and Jerry will provide you the comfort you seek.",
+
+
+
+},
+
+/* Result 6 */
+
+{
+
+"title": "Virgo",
+
+"description": "Stock up on Lenoir cookies!",
+
+
+
+},
+
+/* Result 7 */
+
+{
+
+"title": "Libra",
+
+
+
+"description": "Sing the Alma Mater 10x to yourself in the dorm bathroom mirror",
+
+
+
+},
+
+/* Result 8*/
+
+{
+
+"title": "Scorpio",
+
+"description": "Have a beer (or two).",
+
+
+},
+
+/* Result 9 */
+
+{
+
+"title": "Sagittarius",
+
+"description": "Netflix and Chill with that special someone you've been crushing on.",
+
+
+
+},
+
+/* Result 10 */
+
+{
+
+"title": "Capricorn",
+
+"description": "Hold a fake funeral for our hopes and dreams on the QUAD.",
+
+
+
+},
+
+/* Result 11 */
+
+{
+
+"title": "Aquarius",
+
+"description": "Avoid social media for a week. It'll ease the pain.",
+
+
+
+},
+
+/* Result 12 */
+
+{
+
+"title": "Pisces",
+
+"description": "You're the reason we lost.",
+
+
+
+},
+
+
+
+/*
+
+If you have more than 4 results, copy one of the above sections
+
+and paste it RIGHT AFTER the last one, BEFORE this instruction.
+
+Make sure to copy the whole thing
+
+from the opening bracket { to the closing bracket } and the comma.
+
+*/
+
+],
+
+/*
+
+This section is where all of the questions of the quiz will go.
+
+Each question will be shown in order,
+
+and each answer choice will be shown in order.
+
+In the "result" area below each answer choice,
+
+write the number of the result that this answer corresponds to.
+
+For example, if an answer is for Result 1, put a 1.
+
+There are two kinds of questions:
+
+questions with text answer choices and questions with image answer choices.
+
+The first section has text answers. The second section has image answers.
+
+For the rest of the questions, copy and paste whichever of the 2 you want.
+
+*/
+
+"questions": [
+
+/* This section is for a question with text answer choices */
+
+/* Start copying right AFTER this instruction */
+
+{"question": "What's your sign?",
+
+"answers": [
+
+{
+
+"text": "Aries",
+
+"result": 1
+
+},
+
+{
+
+"text": "Taurus",
+
+"result": 2
+
+},
+
+{
+
+"text": "Gemini",
+
+"result": 3
+
+},
+
+{
+
+"text": "Cancer",
+
+"result": 4
+
+},
+
+{
+
+"text": "Leo",
+
+"result": 5
+
+},
+
+{
+
+"text": "Virgo",
+
+"result": 6
+
+},
+
+{
+
+"text": "Libra",
+
+"result": 7
+
+},
+
+{
+
+"text": "Scorpio",
+
+"result": 8
+
+},
+
+{
+
+"text": "Sagittarius",
+
+"result": 9
+
+},
+
+{
+
+"text": "Capricorn",
+
+"result": 10
+
+},
+
+{
+
+"text": "Aquarius",
+
+"result": 11
+
+},
+
+{
+
+"text": "Pisces",
+
+"result": 12
+
+},
+
+/* If you need more answer choices,
+
+copy and paste a section? from the { to the } and the comma ?
+
+above this instruction, beneath the last choice section. */
+
+]
+},
+
+]
+
+};
