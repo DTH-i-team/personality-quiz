370a56d2a04a6edfd089d8615d8b890469d46a13
mscruser <mscruser@gmail.com>
2016-04-19 20:22:43 -0400

    images-fix

--- sisters.js
+++ sisters.js
@@ -46,15 +46,15 @@ quiz_data = {
         {
         "title": "Tibby",
         "description": "The edgy one of your friend group, you're definitely not afraid to be yourself. You might have a tough exterior, but we all know you really have a heart of gold. ",
-        "img": "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwj0gYGDv5vMAhUMRyYKHZVSA6MQjRwIBw&url=http%3A%2F%2Fwww.fanpop.com%2Fclubs%2Fsisterhood-of-the-traveling-pants%2Fquiz%2Fshow%2F12551%2Fwhat-name-rollins-housekeeper-babysitter-book&psig=AFQjCNGcDUAScjPf7Rc_364Wx6PgRvthrg&ust=1461182420249184",
-	   "source": "fanpop.com",
+        "img": "http://cdn.playbuzz.com/cdn/4688a78c-e1b3-4bd4-8a60-7ee8f0e1b8c2/0ff1ce91-e05b-4370-84cf-a0616a52a08d.jpg",
+	   "source": "playbuzz.com",
         },
 
         /* Result 2 */
         {
         "title": "Bridget",
         "description": "You're super confident and know exactly what you want. Sometimes you run away from your emotions (literally) but you're always there for the people you care about. ",
-        "img": "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwiB24uLwJvMAhWHyyYKHRUBAW0QjRwIBw&url=http%3A%2F%2Fcoolspotters.com%2Fcharacters%2Fbridget-bee-vreeland%2Fand%2Fmovies%2Fsisterhood-of-the-traveling-pants%2Fmedia%2F2295256&bvm=bv.119745492,d.eWE&psig=AFQjCNF810MHVXVHmAu7i8dp01Ne6cgPzg&ust=1461182722045316",
+        "img": "http://coolspotters.com/files/photos/364306/sisterhood-of-the-traveling-pants-gallery.jpg?1357475886",
 	    "source": "coolspotters.com",
         },
 
@@ -62,15 +62,15 @@ quiz_data = {
         {
         "title": "Lena",
         "description": "You might be modest and shy, but your heart is capable of loving fiercely. You're just waiting on the right person to open up to.",
-        "img": "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwjp29nKwJvMAhUBYSYKHXFGCGMQjRwIBw&url=https%3A%2F%2Fwww.pinterest.com%2Frachaelkuck%2Fsisterhood-of-the-traveling-pants%2F&bvm=bv.119745492,d.eWE&psig=AFQjCNG56sc3DFCB6nlwwDvqkjh4ZXtDgA&ust=1461182884304441",
-	    "source": "pinterest.com",
+        "img": "http://www.geocities.ws/hi5sofrzdog/lookuplena.jpg",
+	    "source": "geocities",
         },
 
         /*  Result 4 */
         {
         "title": "Carmen",
         "description": "You love intensely and fight for what you want. You're really smart and you're not afraid to show people how you feel. You are a great friend who really understands other people.",
-        "img": "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwiH5MGKwZvMAhUIRCYKHWIEDkYQjRwIBw&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F371054456771702520%2F&bvm=bv.119745492,d.eWE&psig=AFQjCNH7R9HMmJL68Bcc8RBu8yFHCw8cWg&ust=1461183018143066",
+        "img": "https://s-media-cache-ak0.pinimg.com/564x/2f/f8/25/2ff82581071ce664a449079fc02aa4c6.jpg",
 	    "source": "pinterest.com",
         },
 
